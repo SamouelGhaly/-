e5afe918d3b651bd32fc5ce32530f1c39edabd77
SamouelGhaly <samouelghaly22@gmail.com>
2025-06-20 22:18:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-// طريقة اخري لتغير وسائل التواصل الاجتماعي
-
-function SocialMediaIcons() {
-  const iconMap = {
-    youtube: "fa-youtube",
-    facebook: "fa-facebook",
-    twitter: "fa-twitter",
-    instagram: "fa-instagram"
-  };
-  // حدد كل العناصر <a> اللي جوا الـ LinkList1
-  const links = document.querySelectorAll("#LinkList1 a");
-
-  links.forEach(link => {
-    // خزن النص الأصلي وحوله لحروف صغيرة للتأكد
-    let text = link.textContent.trim().toLowerCase();
-    // هنا بنفحص الكلمة ونغير المحتوى حسبها
-    if (iconMap[text]) {
-      link.innerHTML = `<i class="fa ${iconMap[text]}" aria-hidden="true"></i>`;
-    }
-  });
-}
-// نفذ الدالة بعد تحميل الصفحة
-window.onload = SocialMediaIcons;
-
-
-
-
-
 (function() {
   let executed = false;
 
@@ -64,3 +36,45 @@ document.getElementById("searchToggle").addEventListener("click", function () {
 
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// طريقة اخري لتغير وسائل التواصل الاجتماعي
+
+function SocialMediaIcons() {
+  const iconMap = {
+    youtube: "fa-youtube",
+    facebook: "fa-facebook",
+    twitter: "fa-twitter",
+    instagram: "fa-instagram"
+  };
+  // حدد كل العناصر <a> اللي جوا الـ LinkList1
+  const links = document.querySelectorAll("#LinkList1 a");
+
+  links.forEach(link => {
+    // خزن النص الأصلي وحوله لحروف صغيرة للتأكد
+    let text = link.textContent.trim().toLowerCase();
+    // هنا بنفحص الكلمة ونغير المحتوى حسبها
+    if (iconMap[text]) {
+      link.innerHTML = `<i class="fa ${iconMap[text]}" aria-hidden="true"></i>`;
+    }
+  });
+}
+// نفذ الدالة بعد تحميل الصفحة
+window.onload = SocialMediaIcons;
+
+
